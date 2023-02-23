bab132a331fb16f98690efd1910cef355da75f9a
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-02-23 18:06:03 +0100

    url - fetching API

--- src/Footer/Socials.tsx
+++ src/Footer/Socials.tsx
@@ -1,5 +1,8 @@
 export function Socials() {
     return (
         <div>hej</div>
+        //Skall vara en egen komponent men finns här sålänge
+        //Usestate for this
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=gothenburg&appid=9eaa4e5b0533318eb3e893f94c0d51b7`
     )
 }
