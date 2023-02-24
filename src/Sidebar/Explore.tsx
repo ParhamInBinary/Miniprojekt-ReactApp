60270bdc328e9aba7ed9ae94095429b9b54d5572
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-24 14:07:54 +0100

    Logic for displaying weatherCard after fetcing API + styling
    Co-authored-by: Knightrider85 <Knightrider85@users.noreply.github.com>
    Co-authored-by: Mergim Shala <mergim.shala@medieinstitutet.se>

--- src/Sidebar/Explore.tsx
+++ src/Sidebar/Explore.tsx
@@ -1,22 +1,41 @@
-import { createUseStyles } from 'react-jss';
+import { KeyboardEvent, useState } from "react";
+import { Searchbar } from "./Explore/Searchbar";
+import { WeatherCard } from "./Explore/WeatherCard";
 
-const useStyles = createUseStyles({
-    itemContainer: {
-        padding: '1rem',
-        border: '1px solid #DDD',
-        borderRadius: 20,
-        marginTop: '5rem',
-        backgroundColor: 'rgba(236, 236, 236, 0.4)'
-      },
-    
-  })
+type WeatherData = {
+  name?: string;
+  sys?: { country: string };
+  main?: {
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather?: { main: string }[];
+  cod?: string;
+};
 
 export function Explore() {
-    const styles = useStyles();
-    
-    return (
-        <div className={styles.itemContainer}>
-            
-        </div>
-    )
+  const apiKey = "70ac5e6054972523c1ba9f40bb794ba9";
+  const [weatherData, setWeatherData] = useState<WeatherData>({});
+  const [city, setCity] = useState("");
+
+  const handleSearch = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data: WeatherData) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
+
+  return (
+    <>
+      <Searchbar city={city} setCity={setCity} handleSearch={handleSearch} />
+      <WeatherCard weatherData={weatherData} />
+    </>
+  );
 }
---
+++ src/Sidebar/Explore/Searchbar.tsx
@@ -0,0 +1,32 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  input: {
+    padding: 10,
+    width: "30%",
+    border: "1px solid #DDD",
+    borderRadius: 5,
+    margin: "4rem 0 1rem",
+  },
+});
+
+type SearchbarProps = {
+  city: string;
+  setCity: (city: string) => void;
+  handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+};
+
+export function Searchbar({ city, setCity, handleSearch }: SearchbarProps) {
+  const styles = useStyles();
+
+  return (
+    <input
+      type="text"
+      className={styles.input}
+      placeholder="Enter city..."
+      onChange={(e) => setCity(e.target.value)}
+      value={city}
+      onKeyDown={handleSearch}
+    />
+  );
+}
