bc63b122ff55fd8fd3fbbe51038b168abe85ebb9
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-02-27 12:59:03 +0100

    add function to test if it renders on DOM

---
+++ src/Sidebar/ToggleMode.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+import { createUseStyles } from "react-jss";
+
+
+type Mode = 'dark' | 'light';
+
+export function ToggleMode () {
+  return (
+    <div>
+      <button>hej</button>
+    </div>
+  )
+}
