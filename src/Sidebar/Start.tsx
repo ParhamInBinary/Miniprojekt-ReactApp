e0c57b0bc5fc579a94070363bd345f39b2d155b9
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-27 16:43:44 +0100

    moved some code for styling

--- src/Sidebar/Start.tsx
+++ src/Sidebar/Start.tsx
@@ -1,24 +1,31 @@
 import { createUseStyles } from 'react-jss';
-import { Outlet } from "react-router-dom";
 import { Favorites } from "./Start/Favorites";
 
 export function Start() {
   const styles = useStyles();
 
   return (
+    <>
+    <h1 className={styles.h1}>Welcome to STORM Weather App</h1>
+    <p>By PAMEDA</p>
     <div className={styles.itemContainer}>
       <Favorites />
-      <Outlet />
     </div>
+    </>
   );
 }
 
 const useStyles = createUseStyles({
   itemContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
     padding: '1rem',
     border: '1px solid #DDD',
     borderRadius: 20,
     marginTop: '5rem',
     backgroundColor: 'rgba(236, 236, 236, 0.4)'
+  },
+  h1: {
+    marginTop: '3rem'
   },
 })
