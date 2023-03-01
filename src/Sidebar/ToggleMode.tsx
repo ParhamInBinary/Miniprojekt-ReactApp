83be844ad8889660a2d1298f53a36097fe3d6fdf
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-01 10:21:04 +0100

    import react-switch

--- src/Sidebar/ToggleMode.tsx
+++ src/Sidebar/ToggleMode.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
+import ReactSwitch from "react-switch";
 
 const useStyles = createUseStyles({
   lightTheme: {
